eb14d3308192f8bb0cc9ce94febd92e928933a94
William Alderman <125694577+Lov3M4chine@users.noreply.github.com>
2023-06-12 12:10:23 -0600

    Add test to access temperature data

--- src/index.js
+++ src/index.js
@@ -4,4 +4,11 @@ import './styles.css';
 let latitude = 39.61;
 let longitude = -105.02;
 
-fetchWeatherData(latitude, longitude);
+fetchWeatherData(latitude, longitude)
+    .then(weatherData => {
+        let temperature = weatherData.current_weather.temperature;
+        console.log(`temperature: ${temperature}`);
+    })
+    .catch(error => {
+        console.error('Error fetching weather data:', error);
+    });
